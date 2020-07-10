70ad8250f506aa8c52e4a4943dc7d1172d964be3
nalina26 <alina.birlog93@gmail.com>
2020-07-10 11:02:30 +0300

    Add files via upload

---
+++ resources/orase2.sql
@@ -0,0 +1,37 @@
+-- MySQL dump 10.13  Distrib 5.5.24, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: orase
+-- ------------------------------------------------------
+-- Server version	5.5.24-0ubuntu0.12.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `orase`
+--
+
+LOCK TABLES `orase` WRITE;
+/*!40000 ALTER TABLE `orase` DISABLE KEYS */;
+INSERT INTO `orase` VALUES (23.57,46.07,'Alba Iulia','Alba','AB',65091,'Centru'),(23.58,46.10,'Barabant','Alba','AB',2145,'Centru'),(23.56,46.10,'Micesti','Alba','AB',1420,'Centru'),(23.59,46.02,'Oarda','Alba','AB',1830,'Centru'),(23.52,46.05,'Paclisa','Alba','AB',682,'Centru'),(23.61,46.05,'Ciugud','Alba','AB',423,'Centru'),(23.63,46.08,'Drambar','Alba','AB',372,'Centru'),(23.60,46.04,'Limba','Alba','AB',290,'Centru'),(23.67,46.08,'Hapria','Alba','AB',507,'Centru'),(23.63,46.06,'Seusa','Alba','AB',606,'Centru'),(23.63,46.09,'Teleac','Alba','AB',385,'Centru'),(23.07,46.27,'Abrud','Alba','AB',4820,'Centru'),(23.06,46.28,'Abrud-Sat','Alba','AB',1355,'Centru'),(23.09,46.26,'Gura Cornei','Alba','AB',291,'Centru'),(23.03,46.28,'Soharu','Alba','AB',263,'Centru'),(23.72,46.31,'Aiud','Alba','AB',24619,'Centru'),(23.69,46.32,'Aiudul de Sus','Alba','AB',2276,'Centru'),(23.72,46.35,'Gambas','Alba','AB',578,'Centru'),(23.65,46.34,'Magina','Alba','AB',628,'Centru'),(23.75,46.34,'Pagida','Alba','AB',165,'Centru'),(23.76,46.31,'Ciumbrud','Alba','AB',1504,'Centru'),(23.67,46.26,'Garbova de Jos','Alba','AB',464,'Centru'),(23.62,46.29,'Garbova de Sus','Alba','AB',357,'Centru'),(23.64,46.28,'Garbovita','Alba','AB',164,'Centru'),(23.75,46.29,'Sancrai','Alba','AB',1023,'Centru'),(23.69,46.26,'Tifra','Alba','AB',116,'Centru'),(23.92,46.18,'Blaj','Alba','AB',15714,'Centru'),(23.85,46.10,'Deleni-Obarsie','Alba','AB',5,'Centru'),(23.87,46.11,'Flitesti','Alba','AB',4,'Centru'),(23.90,46.17,'Izvoarele','Alba','AB',2078,'Centru'),(23.92,46.19,'Petrisat','Alba','AB',316,'Centru'),(23.89,46.16,'Tiur','Alba','AB',1765,'Centru'),(23.92,46.17,'Veza','Alba','AB',1568,'Centru'),(23.97,46.14,'Manarade','Alba','AB',865,'Centru'),(23.94,46.14,'Spatac','Alba','AB',110,'Centru'),(23.05,46.37,'Campeni','Alba','AB',5704,'Centru'),(23.07,46.36,'Boncesti','Alba','AB',165,'Centru'),(23.01,46.41,'Borlesti','Alba','AB',141,'Centru'),(23.01,46.38,'Botesti','Alba','AB',158,'Centru'),(23.03,46.41,'Certege','Alba','AB',97,'Centru'),(23.05,46.41,'Coasta Vascului','Alba','AB',133,'Centru'),(23.09,46.36,'Dandut','Alba','AB',116,'Centru'),(23.06,46.38,'Dealu Bistrii','Alba','AB',122,'Centru'),(23.07,46.41,'Dealu Capsei','Alba','AB',318,'Centru'),(23.05,46.40,'Dric','Alba','AB',154,'Centru'),(23.06,46.35,'Fata Abrudului','Alba','AB',161,'Centru'),(23.02,46.39,'Floresti','Alba','AB',179,'Centru'),(23.08,46.34,'Furduiesti','Alba','AB',96,'Centru'),(23.03,46.37,'Mihoesti','Alba','AB',186,'Centru'),(23.04,46.38,'Motorasti','Alba','AB',108,'Centru'),(23.03,46.44,'Peste Valea Bistrii','Alba','AB',61,'Centru'),(23.05,46.35,'Poduri','Alba','AB',152,'Centru'),(23.02,46.37,'Sorlita','Alba','AB',35,'Centru'),(23.01,46.45,'Tomusesti','Alba','AB',85,'Centru'),(23.07,46.38,'Valea Bistrii','Alba','AB',379,'Centru'),(23.03,46.40,'Valea Caselor','Alba','AB',141,'Centru'),(23.07,46.36,'Varsi','Alba','AB',187,'Centru'),(23.37,45.84,'Cugir','Alba','AB',28780,'Centru'),(23.43,45.77,'Bocsitura','Alba','AB',87,'Centru'),(23.40,45.79,'Bucuru','Alba','AB',141,'Centru'),(23.44,45.83,'Calene','Alba','AB',121,'Centru'),(23.35,45.81,'Feteni','Alba','AB',119,'Centru'),(23.46,45.78,'Goasele','Alba','AB',37,'Centru'),(23.45,45.82,'Mugesti','Alba','AB',152,'Centru'),(23.35,45.89,'Vinerea','Alba','AB',2440,'Centru'),(23.85,46.39,'Ocna Mures','Alba','AB',11009,'Centru'),(23.84,46.37,'Uioara de Jos','Alba','AB',1345,'Centru'),(23.88,46.39,'Uioara de Sus','Alba','AB',1415,'Centru'),(23.81,46.38,'Cisteiu de Mures','Alba','AB',643,'Centru'),(23.79,46.37,'Micoslaca','Alba','AB',336,'Centru'),(23.87,46.41,'Razboieni-Cetate','Alba','AB',1508,'Centru'),(23.57,45.96,'Sebes','Alba','AB',23227,'Centru'),(23.56,45.99,'Lancram','Alba','AB',1487,'Centru'),(23.56,45.92,'Petresti','Alba','AB',4120,'Centru'),(23.62,45.90,'Rahau','Alba','AB',920,'Centru'),(23.22,46.11,'Zlatna','Alba','AB',4446,'Centru'),(23.15,46.18,'Botesti','Alba','AB',113,'Centru'),(23.13,46.19,'Budeni','Alba','AB',88,'Centru'),(23.15,46.12,'Dealu Roatei','Alba','AB',49,'Centru'),(23.14,46.17,'Dobrot','Alba','AB',30,'Centru'),(23.19,46.14,'Dumbrava','Alba','AB',16,'Centru'),(23.30,46.10,'Fenes','Alba','AB',1163,'Centru'),(23.29,46.09,'Galati','Alba','AB',465,'Centru'),(23.17,46.15,'Izvoru Ampoiului','Alba','AB',486,'Centru'),(23.26,46.09,'Patrangeni','Alba','AB',412,'Centru'),(23.16,46.14,'Pirita','Alba','AB',163,'Centru'),(23.19,46.16,'Parau Gruiului','Alba','AB',238,'Centru'),(23.25,46.10,'Podu lui Paul','Alba','AB',65,'Centru'),(23.22,46.19,'Runc','Alba','AB',52,'Centru'),(23.11,46.21,'Rusi','Alba','AB',66,'Centru'),(23.25,46.09,'Suseni','Alba','AB',168,'Centru'),(23.15,46.15,'Trampoiele','Alba','AB',593,'Centru'),(23.28,46.08,'Valea Mica','Alba','AB',517,'Centru'),(23.22,46.14,'Valtori','Alba','AB',261,'Centru'),(22.97,46.45,'Albac','Alba','AB',524,'Centru'),(22.99,46.46,'Barasti','Alba','AB',209,'Centru'),(22.97,46.48,'Budaiesti','Alba','AB',115,'Centru'),(22.99,46.47,'Cionesti','Alba','AB',119,'Centru'),(22.91,46.49,'Costesti','Alba','AB',150,'Centru'),(22.99,46.49,'Dealu Lamasoi','Alba','AB',263,'Centru');
+/*!40000 ALTER TABLE `orase` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2012-09-28 10:47:41
